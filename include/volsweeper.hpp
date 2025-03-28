5c59f0276c7d84e6d390d121b77f0913732e07ee
RobbyLawrence <rwl19291@gmail.com>
2025-03-28 16:58:45 -0400

    working on updating mine-adjacent squares

--- include/volsweeper.hpp
+++ include/volsweeper.hpp
@@ -1,8 +1,6 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <random>
-#include <unordered_map>
 // we won't use "using namespace std" because
 // that's bad form when making header files
 
@@ -15,44 +13,83 @@ struct Minefield {
     std::vector<std::vector<int> > grid;
     Minefield(std::string flag, size_t size_c, size_t num_mines_c) {
         // we'll assume num_mines is positive, so error-checking is required in the main file
+        // // each tile is represented in the map, and 0 means a mine can be placed, 1 means no
+        // a mine can go anywhere, as long as it isn't surrounded fully by mines
+        // so we run a check to see if a tile is surrounded by mines
+        /* Board can be visualized like this:
+        _____________
+        | 0 | 1 | 2 |
+        -------------
+        | 3 | 4 | 5 |
+        -------------
+        | 6 | 7 | 8 |
+        -------------
+        We assume all boards are squares.
+        The number in each square represents the "ID" of the tile. To get the grid vector entry for an ID x,
+        use grid[floor(x/size)][x % size] (i came up with these equations, hopefully they work). We can use
+        type casting to integer for the floor function.
+        */
         num_mines = num_mines_c;
         size = size_c;
         std::string flag_string;
         if (flag == "-r") {
-            std::random_device rd; // these two lines are for <random>
-            std::mt19937 gen(rd());
-            // tracker tracks whether or not a tile is available to be made a mine
-            std::unordered_map<int,int> tracker; // tracker to keep track of cells that can be filled
-            for (size_t i = 0;i<size*size-1;i++) {
-                tracker.insert(std::make_pair(i,0));
-            }
-            // each tile is represented in the map, and 0 means a mine can be placed, 1 means no
-            // a mine can go anywhere, as long as it isn't surrounded fully by mines
-            // so we run a check to see if a tile is surrounded by mines
-            /* Board can be visualized like this:
-            _____________
-            | 0 | 1 | 2 |
-            -------------
-            | 3 | 4 | 5 |
-            -------------
-            | 6 | 7 | 8 |
-            -------------
-            We assume all boards are squares.
-            The number in each square represents the "ID" of the tile. To get the grid vector entry for an ID x,
-            use grid[floor(x/size)][x % size] (i came up with these equations, hopefully they work). We can use
-            type casting to integer for the floor function.
-
-            */
-            std::uniform_int_distribution<int> dist(0,size*size-1);
             grid.resize(size,std::vector<int>(size,0));
-            int random_number;
-            for (size_t i = 0;i<num_mines;i++) {
-                random_number = dist(gen);
-                if (grid[(int)random_number/size][random_number % size] == -1) {
-                    i--; // if the randomly selected number is a mine,
-                    continue;
+            std::vector<int> mine_ids(num_mines);
+            std::vector<int> available_numbers(size*size);
+            std::iota(available_numbers.begin(), available_numbers.end(), 0);
+
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::shuffle(available_numbers.begin(), available_numbers.end(), gen);
+            std::copy_n(available_numbers.begin(), num_mines, mine_ids.begin());
+            /*
+            The above code generates a vector that has num_mines unique random integers using <random>.
+            Below, we use these IDs to fill the grid with mines.
+            */
+            for (size_t i = 0;i<mine_ids.size();i++) {
+                grid[(int)(i / size)][i % size] = -1;
+            }
+            // the grid is now filled with mines, and we want to update the squares close to it.
+            for (int i = 0;i<size;i++) {
+                for (int j = 0;j<size;j++) {
+                    if (grid[i][j] == -1) {
+                        continue;
+                    }
+                    if (i == 0 && j == 0) { // top left corner
+                        // process
+                        continue;
+                    }
+                    if (i == 0 && j == size - 1) { // top right corner
+                        // process
+                        continue;
+                    }
+                    if (i == size - 1 && j == 0) { // bottom left corner
+                        // process
+                        continue;
+                    }
+                    if (i == size - 1 && j == size - 1) { // bottom right corner
+                        // process
+                        continue;
+                    }
+                    if (i == 0) { // top wall
+                        // process
+                        continue;
+                    }
+                    if (j == 0) { // left wall
+                        // process
+                        continue;
+                    }
+                    if (j == size - 1) { // right wall
+                        // process
+                        continue;
+                    }
+                    if (i == size - 1) { // bottom wall
+                        // process
+                        continue;
+                    }
                 }
             }
+
         }
     }
 };
