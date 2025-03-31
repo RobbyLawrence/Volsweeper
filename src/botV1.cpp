75c35379ecbda67dbd86352ed545de788d4d05e7
Brett William Dowling <bdowlin2@hydra10.eecs.utk.edu>
2025-03-31 13:24:26 -0400

    Added BotV1 with outline

---
+++ src/botV1.cpp
@@ -0,0 +1,40 @@
+//Version one of the pattern recognizing bot to show playable moves
+
+
+
+
+
+//Basic Patterns section
+
+//B1
+//If the number of the square is the same as the adjacent squares then all of the squares are mines
+
+//B2
+//If the number is touching the same number of flags then all adjacent squares can be opened
+
+//1-1
+//Look at the left 1. It touches the two yellow cells, so they contain one mine. Now look at the right 1.
+//It also touches the two yellow cells, so it already has a mine and the third cell can be opened.
+
+//1-1+
+//
+
+//1-2
+//
+
+//1-2+
+//
+
+
+//1-2C
+//
+
+//1-2C+
+//
+
+//1-2-1
+//
+
+//1-2-2-1
+//
+
