f489f06901314831fed7815d286696fa6b7a4f84
RobbyLawrence <rwl19291@gmail.com>
2025-03-30 23:08:17 -0400

    finishes board generation and mine placement

--- src/game.cpp
+++ src/game.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include "volsweeper.hpp"
-using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        cerr << "usage: ./volsweeper [FLAG] [SIZE]\n";
+    if (argc != 4) {
+        std::cerr << "usage: ./volsweeper [FLAG] [SIZE] [NUM_MINES]\n";
+        return 1;
+    }
+    int size;
+    int num_mines;
+    try {
+        size = std::stoi(argv[2]);
+    } catch (const std::invalid_argument& error) {
+        std::cerr << "Invalid argument: " << error.what() << std::endl;
+        return 1;
+    } catch (const std::out_of_range& error) {
+        std::cerr << "Out of range: " << error.what() << std::endl;
         return 1;
     }
 
+    try {
+        num_mines = std::stoi(argv[3]);
+    } catch (const std::invalid_argument& error) {
+        std::cerr << "Invalid argument: " << error.what() << std::endl;
+        return 1;
+    } catch (const std::out_of_range& error) {
+        std::cerr << "Out of range: " << error.what() << std::endl;
+        return 1;
+    }
+    Minefield field = Minefield(argv[1],size,num_mines);
+    field.output_field();
     return 0;
 }
