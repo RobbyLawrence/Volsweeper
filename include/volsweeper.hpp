9cecdfbfdd1e7d60b47268012482402e0b675d9a
Matthew Mark Ferrari <dwp393@hydra11.eecs.utk.edu>
2025-03-31 12:22:30 -0400

    test to see if gui dir is added

--- include/volsweeper.hpp
+++ include/volsweeper.hpp
@@ -1,17 +1,16 @@
-#include <vector>
 #include <algorithm>
 #include <random>
+#include <vector>
 // we won't use "using namespace std" because
 // that's bad form when making header files
 
-
 // struct for generating fields
 struct Minefield {
-    size_t size;
-    size_t num_mines;
-    // entry will be -1 for mines and otherwise represent number of mines adjacent
-    std::vector<std::vector<int> > grid;
-    void increment_grid_entry(int i, int j);
-    Minefield(std::string flag, size_t size_c, size_t num_mines_c);
-    void output_field();
+  size_t size;
+  size_t num_mines;
+  // entry will be -1 for mines and otherwise represent number of mines adjacent
+  std::vector<std::vector<int>> grid;
+  void increment_grid_entry(int i, int j);
+  Minefield(std::string flag, size_t size_c, size_t num_mines_c);
+  void output_field();
 };
